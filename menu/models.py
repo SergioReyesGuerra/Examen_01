382be36c2decaa03677402298241be89fa43f565
Sergio <sergio.reyes@gmail.com>
2025-04-09 14:17:03 -0500

    Paso 3: Crear modelo Plato

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Plato(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
